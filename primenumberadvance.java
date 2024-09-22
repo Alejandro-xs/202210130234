41946aed0961537e795c2821280ed751ae8adcc2
tanyuan <14932939+tanyuanxx@user.noreply.gitee.com>
2024-09-22 16:23:34 +0800

    更新 primenumberadvance.java

--- primenumberadvance.java
+++ primenumberadvance.java
@@ -2,10 +2,10 @@ public class primenumberadvance {
     public static void main(String[] args) {
         int limit = 20000;
         boolean[] isComposite = new boolean[limit + 1];
-        int primeCount = 0; // 更具描述性的命名
+        int primeCount = 0; 
         for (int i = 2; i * i <= limit; i++) {
             if (!isComposite[i]) { 
-                for (int j = i * i; j < isComposite.length; j += i) { // 修正Array越界
+                for (int j = i * i; j < isComposite.length; j += i) { 
                     isComposite[j] = true; 
                 }
             }

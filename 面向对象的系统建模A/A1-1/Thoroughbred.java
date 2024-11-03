1eaa9aab98ca1a4f3c49d6fcf9f0586dd606b81a
Alejandro-xs <hzx20040117@gmail.com>
2024-11-03 14:47:11 +0800

    Add files via upload

---
+++ 面向对象的系统建模A/A1-1/Thoroughbred.java
@@ -0,0 +1,34 @@
+package A1_1;
+
+public class ThoroughbredHorse {
+    private String motherName;
+    private String fatherName;
+    private Integer yearOfBirth;
+
+    public ThoroughbredHorse() {
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public Integer getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(Integer yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
